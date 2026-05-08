f90b55c44f0ea8ec1b6ffacbbe5f81284e8c713d
bigmalove <125801848+bigmalove@users.noreply.github.com>
2026-05-08 11:02:26 +0800

    Update index.ts

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import { logger } from "./lib/logger";
+import { startRcapiRegistration } from "./lib/reapi-registration";
 
 const rawPort = process.env["PORT"];
 
@@ -22,6 +23,7 @@ const server = app.listen(port, (err) => {
   }
 
   logger.info({ port }, "Server listening");
+  startRcapiRegistration();
 });
 
 // Allow model generation requests up to 600 seconds
